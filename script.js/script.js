9ba8637e8c4458fb90575760912c3c62c928609b
Lucas Cardoso <lucashenrique0221@gmail.com>
2022-01-17 19:51:40 -0300

    Adicionado navegação para mobile

--- script.js/script.js
+++ script.js/script.js
@@ -1,1 +1,6 @@
-//alert("Ola");
+const navButton = document.getElementById('mobile-nav')
+const sidebar = document.getElementById('sidebar')
+
+navButton.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar-active')
+})
