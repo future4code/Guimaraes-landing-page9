629b74a2f0b9c677e92ce1da0c23f8a0a4d0ae3a
Lucas Cardoso Padocaking <lucascardoso0221@gmail.com>
2022-01-20 19:52:43 -0300

    Adicionado página "Mercado"

--- script.js/script.js
+++ script.js/script.js
@@ -1,10 +1,15 @@
+/* HEADER */
 const navButton = document.getElementById('mobile-nav')
 const sidebar = document.getElementById('sidebar')
 
 navButton.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar-active')
 })
+/* HEADER */
 
+
+
+/* SLIDES */
 let totalSlides = document.querySelectorAll('.slider--item').length;
 let currentSlide = 0;
 
@@ -41,3 +46,4 @@ function updateMargin() {
 }
 
 setInterval(goNext, 3000);
+/* SLIDES */
